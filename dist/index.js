c9d0b8e0b828fe229ea1ef8279f829afed6dac14
Floyd-88 <ilasaz1988@gmail.com>
2023-09-22 19:53:34 +0300

    text message

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 80
 
 app.get('/', function(req, res) {
-    res.send('Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    res.send('My APP Express js')
 })
 
 app.listen(port, () => {
